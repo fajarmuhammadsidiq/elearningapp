bef89992d36ab48d2d46cf44095d16b84d346994
fajarmuhammadsidiq <fajars328@gmail.com>
2023-05-18 09:30:07 +0700

    create model

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import 'firebase_options.dart';
+import 'screens/data_uploader_screen.dart';
 
-void main(List<String> args) async {
+Future<void> main(List<String> args) async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -15,8 +18,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
+    return GetMaterialApp(
+      home: DataUploaderScreen(),
     );
   }
 }
---
+++ lib/screens/data_uploader_screen.dart
@@ -0,0 +1,16 @@
+import 'package:elearning_app/controllers/question_paper/data_uploader.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class DataUploaderScreen extends StatelessWidget {
+  DataUploaderScreen({super.key});
+  DataUploader controller = Get.put(DataUploader());
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("uploading"),
+      ),
+    );
+  }
+}
